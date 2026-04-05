11ef7d5671e240c4cdbfc7ac79d7d69a919738a4
harinathbandi144 <harinath.bandi144@gmail.com>
2026-04-05 23:47:47 +0530

    Data Tranformations

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,13 +7,13 @@ from sklearn.model_selection import train_test_split
 
 from src.exception import CustomException
 from src.logger import logging
+from src.components.data_tranformation import DataTranformations
 
 
 # -------------------- CONFIG CLASS --------------------
-# Stores all output file paths in one place
 @dataclass
 class DataIngestionConfig:
-    artifacts_dir: str = "Artifacts"
+    artifacts_dir: str = "artifacts"
     train_data_path: str = os.path.join(artifacts_dir, "train.csv")
     test_data_path: str = os.path.join(artifacts_dir, "test.csv")
     raw_data_path: str = os.path.join(artifacts_dir, "data.csv")
@@ -28,33 +28,25 @@ class DataIngestion:
         logging.info("Entered the data ingestion component")
 
         try:
-            # ✅ Build dataset path relative to THIS file (production-safe way)
-            current_dir = os.path.dirname(__file__)              # src/components
-            src_dir = os.path.abspath(os.path.join(current_dir, ".."))  # src
+            current_dir = os.path.dirname(__file__)
+            src_dir = os.path.abspath(os.path.join(current_dir, ".."))
             dataset_path = os.path.join(
                 src_dir, "notebook", "data", "StudentsPerformance.csv"
             )
 
             print("Reading dataset from:", dataset_path)
 
-            # Read dataset
             df = pd.read_csv(dataset_path)
             logging.info("Dataset loaded into DataFrame")
 
-            # Create Artifacts folder
             os.makedirs(self.ingestion_config.artifacts_dir, exist_ok=True)
 
-            # Save raw data
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
-            logging.info("Train-Test split initiated")
-
-            # Train test split
             train_set, test_set = train_test_split(
                 df, test_size=0.2, random_state=42
             )
 
-            # Save train and test
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
@@ -66,70 +58,14 @@ class DataIngestion:
             )
 
         except Exception as e:
-            print("ERROR:", e)  # temporary debug visibility
+            print("ERROR:", e)
             raise CustomException(e, sys)
 
 
 # -------------------- RUN AS MODULE --------------------
-# Run using:
-# python -m src.components.data_ingestion
 if __name__ == "__main__":
     obj = DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data, test_data = obj.initiate_data_ingestion()
 
-
-
-
-
-
-
-# import os
-# import sys
-# from src.exception import CustomException
-# from src.logger import logging
-# import pandas as pd
-# from sklearn.model_selection import train_test_split
-# from dataclasses import dataclass
-
-# @dataclass
-# class DataIngestionConfig:
-#     train_data_path: str = os.path.join('Artifacts', 'train.csv')
-#     test_data_path: str = os.path.join('Artifacts', 'test.csv')
-#     raw_data_path: str = os.path.join('Artifats', 'data.csv')
-
-# class DataIngestion:
-#     def __init__(self):
-#         self.ingestion_config = DataIngestionConfig()
-
-#     def initiate_data_ingestion(self):
-#         logging.info("Entered the data ingestion method or component")
-
-#         try:
-#             df = pd.read_csv("notebook/data/StudentsPerformance.csv")
-#             logging.info("Exported the red the data set as Data frame")
-
-#             os.makedirs(os.path.dirname(self.ingestion_config.test_data_path),exist_ok=True) # we are creating directory/artifact of train data
-            
-#             df.to_csv(self.ingestion_config.raw_data_path, index= False, header=True)
-
-#             logging.info("Train Test Split Initiated")
-#             train_set, test_set = train_test_split(df, test_size= 0.2, random_state=42)
-
-#             train_set.to_csv(self.ingestion_config.train_data_path, index = False, header = True)
-#             test_set.to_csv(self.ingestion_config.test_data_path, index = False, header = True)
-
-#             logging.info("Ingestion of the data is completed")
-
-#             return(
-#                 self.ingestion_config.train_data_path,
-#                 self.ingestion_config.test_data_path
-#             )
-#         except Exception as e:
-#             raise CustomException(e, sys)
-    
-# if __name__=='__main__':
-#     obj = DataIngestion()
-#     obj.initiate_data_ingestion()
-
-
-            
+    data_tranformation = DataTranformations()
+    data_tranformation.initiate_data_tranformations(train_data, test_data)
--- src/exception.py
+++ src/exception.py
@@ -6,7 +6,7 @@ from src.logger import logging
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info() # i want only 3rd information not first 2 
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = " Error occured in python script name [{o}] line number [{1}] error message is [{2}]".format(
+    error_message = " Error occured in python script name [{0}] line number [{1}] error message is [{2}]".format(
        file_name,exc_tb.tb_lineno,str(error))
 
     return error_message
